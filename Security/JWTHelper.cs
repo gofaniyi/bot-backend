b657d09c5e3e9716dc4c97d40508b697e5decb0d
Taiwo insight <taiwo.insight@gmail.com>
2020-03-24 11:16:53 +0100

    Added support for SignalR

---
+++ Security/JWTHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using static GloEpidBot.Model.Domain.TokenOption;
+
+namespace GloEpidBot.Security
+{
+    public static class JWTHelper
+    {
+        public static TokenReturnHelper GetJWTToken(string UserMail, string UserId,  TokenOptions option)
+        {
+            var claims = new List<Claim>()
+           {
+               new Claim(JwtRegisteredClaimNames.Sub, UserMail),
+               new Claim("UserId", UserId),
+               new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+               new Claim("Email",UserMail)
+           };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(option.Secret));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(Convert.ToDouble(option.accessExpiration));
+
+            var token = new JwtSecurityToken(option.Issuer, option.Audience, claims, expires: expires, signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return new TokenReturnHelper
+            {
+                ExpiresIn = expires,
+                Token = jwt
+            };
+
+
+        }
+
+        public class TokenReturnHelper
+        {
+            public string Token { get; set; }
+            public DateTime ExpiresIn { get; set; }
+        }
+    }
+}
+
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using GloEpidBot.Model.Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +13,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using static GloEpidBot.Model.Domain.TokenOption;
 
 namespace GloEpidBot
 {
@@ -26,6 +31,38 @@ namespace GloEpidBot
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSignalR();
+          
+            services.AddCors(o => o.AddPolicy("All", b => b.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
+
+
+       
+
+
+            var token = Configuration.GetSection("tokenManagement").Get<TokenOptions>();
+            var secret = Encoding.ASCII.GetBytes(token.Secret);
+            services.AddSingleton(token);
+
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(token.Secret)),
+                    ValidIssuer = token.Issuer,
+                    ValidAudience = token.Audience,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+               
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
